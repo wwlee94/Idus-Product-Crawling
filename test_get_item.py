07397494e18626ffa103c475fd579042419e9c3f
wwlee94 <wwlee94@naver.com>
2019-11-22 17:48:36 +0900

    test 코드생성  및 데이터 1000개 csv파일로 저장

---
+++ test_get_item.py
@@ -0,0 +1,15 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
+import io
+import csv
+import re
+
+rf = io.open ('idus_item_list.csv','rb')
+reader = csv.reader(rf)
+for item in reader:
+    print(item[1])
+
+rf.close()
+
+
