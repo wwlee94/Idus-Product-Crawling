98abe0a2fe08ff823e2f1297d8aec401ed83f4ea
wwlee94 <wwlee94@naver.com>
2019-11-23 17:55:05 +0900

    csv파일 읽어 list 형태의 문자열 파싱해 가져오는 테스트

--- test_get_item.py
+++ test_get_item.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 import requests
 import bs4
 from bs4 import BeautifulSoup
@@ -5,12 +7,17 @@ import io
 import csv
 import re
 
-
+# 테스트 : csv 파일 리스트값 (썸네일_리스트_320) 가져오기
 rf = io.open ('idus_item_list.csv','rb')
 reader = csv.reader(rf)
+index = 0
 for item in reader:
-    print(item[1])
-
+    if index != 0: 
+        split_str = item[3].split(',')
+        for string in split_str:
+            print(re.sub("[\[\]\'\s]",'',string))
+        print('')
+    index += 1
 rf.close()
 
 
